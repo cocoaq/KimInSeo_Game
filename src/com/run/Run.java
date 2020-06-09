15bf1f58c319622b891908c4b603053c63ced3da
인서 <@DESKTOP-J836M3E>
2020-06-09 18:46:40 +0900

    tlehgkqslek

---
+++ src/com/run/Run.java
@@ -0,0 +1,16 @@
+package com.run;
+
+import java.util.Scanner;
+
+public class Run {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("게임을 시작합니다.");
+		System.out.println("당신의 이름은? : ");
+		
+		
+	}
+
+}
